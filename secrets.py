7986f809b442cd8a4e809534dff6548734e342d6
Artemprod <57044749+Artemprod@users.noreply.github.com>
2022-06-27 18:30:58 +0300

    Update secrets.py
    change secret password

--- secrets.py
+++ secrets.py
@@ -2,8 +2,8 @@ from pydantic import BaseSettings, Field
 
 
 class Secrets(BaseSettings):
-    token: str = Field(default="5193936594:AAESI-SFziCVSzSnl7b7Zz3leB7swYNwL08", env="TOKEN")
-    bd_password: str = Field(default="Portnov1991", env="DB_PASSWORD")
+    token: str = Field(default=..., env="TOKEN")
+    bd_password: str = Field(default=..., env="DB_PASSWORD")
     bd_host: str = Field(default="127.0.0.1", env="DB_HOST")
     bd_port: int = Field(default=5432, env="DB_PORT")
     bd_user: str = Field(default="postgres", env="DB_USER")
